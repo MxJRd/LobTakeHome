3c07929430bca9fc7c593d1f86c97661fc94fab2
MXJRD <reed182@mail.chapman.edu>
2021-07-16 16:12:32 -0700

    Added extra functionalities.

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -8,22 +8,71 @@ for(let i = 0; i < db.length; i += 1) { //assign simple ids
   elem.id = i;
 }
 
-const findAddress = (address) => {
+const addressMatches = (address) => {
   return db.filter(elem => {
     return __.isEqual(elem, address);
    });
 }
 
-apiRouter.get("/", (req, res) => { //READ
+// const getIndex = (req) => {
+//   return db.findIndex(elem => {
+//     return elem.id === Number(req.params.id);
+//   })
+// }
+
+apiRouter.get("/addresses/:pgNum", (req, res) => { //READ
+  const pgNum = req.params.pgNum;
   try {
-    console.log(db)
-    res.status(200).send(db);
+    //3 results per page
+    //pg0 0 3
+    //pg1 3 6
+    //pg2 6 9
+    const startIdx = Number(pgNum) * 3;
+    const results = db.slice(startIdx,  startIdx + 3);
+    res.status(200).send(results);
   } catch (e) {
     console.log(e);
   }
 });
 
-apiRouter.post("/", (req, res) => { //CREATE
+apiRouter.get("/addresses/:query", (req, res) => {
+
+  try {
+    // console.log("request is firing")
+    const queryString = req.params.query,
+          results = [];
+    for(const address of db) {
+      const allKeys = Object.keys(address);
+      for(key of allKeys) {
+        // console.log(address[key]);
+        if(key !== "id" && address[key].includes(queryString)) {
+          results.push(address);
+          break;
+        };
+      }
+    }
+    const stringifiedResults = JSON.stringify(results);
+    res.status(200).send(stringifiedResults);
+  } catch(e) {
+    console.log(e);
+  }
+})
+
+apiRouter.get("/addresses/:id", (req, res) => { //Find a specific address
+  try {
+    // const index = db.findIndex(elem => {
+    //   return elem.id === Number(req.params.id);
+    // })
+    const index = db.findIndex(elem => {
+      return elem.id === Number(req.params.id);
+    });
+    res.status(200).send(db[index]);
+  } catch(e) {
+    console.log(e);
+  }
+})
+
+apiRouter.post("/addresses", (req, res) => { //CREATE
   try {
     const { line1, line2, city, state, zip } = req.body;
     if(typeof line1 !== "string" || typeof city !== "string" || typeof state !== "string"|| typeof zip !== "string") {
@@ -40,7 +89,7 @@ apiRouter.post("/", (req, res) => { //CREATE
     if(line2) {
       newAddress[line2] = line2;
     }
-    const items = findAddress(newAddress);
+    const items = addressMatches(newAddress);
     db.push(newAddress);
     if(items.length > 0) {
       res.sendStatus(200);
@@ -52,7 +101,7 @@ apiRouter.post("/", (req, res) => { //CREATE
   }
 });
 
-apiRouter.put("/:id", (req, res) => { //UPDATE
+apiRouter.put("/addresses/:id", (req, res) => { //UPDATE
   try {
     const { line1, line2, city, state, zip } = req.body;
     if(typeof line1 !== "string" || typeof city !== "string" || typeof state !== "string"|| typeof zip !== "string") {
@@ -65,18 +114,22 @@ apiRouter.put("/:id", (req, res) => { //UPDATE
       zip,
       id: Number(req.params.id)
     };
+    if(line2) {
+      newAddress[line2] = line2;
+    }
     const index = db.findIndex(elem => {
       return elem.id === Number(req.params.id);
     })
     db[index] = updatedAddress;
     console.log("db: ", db);
-    res.status(200).send(`Updated Item(s)`);
+    const stringifiedDB = JSON.stringify(db);
+    res.status(200).send(`Updated Item: \n ${stringifiedDB}`);
   } catch(e) {
     console.log(e);
   }
 })
 
-apiRouter.delete("/delete", (req, res) => { //DELETE
+apiRouter.delete("/addresses/", (req, res) => { //DELETE
   try {
     const { id } = req.body;
     if(typeof id !== "string") {
